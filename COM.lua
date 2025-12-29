21fd3de2fc026affebbca8c1ba5b38de9bd066f7
cichonowek <cichonsylwek.sc@gmail.com>
2025-12-29 14:07:47 +0100

    Update COM.lua

--- COM.lua
+++ COM.lua
@@ -1,47 +1,41 @@
 if queue_on_teleport then queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/cichonowek/HACK/refs/heads/master/COM.lua")) end
 
-local CoreGui = game:GetService("CoreGui")
-
-if CoreGui:FindFirstChild("MobileActivatorGUI") then
-    CoreGui.MobileActivatorGUI:Destroy()
-end
-
 local function SetGcValue(str, val)
-    for _, Table in next, getgc(true) do
+
+    local garbage = getgc(true)
+
+    for i, Table in next, garbage do
+
         if type(Table) == "table" then
-            if table.isfrozen(Table) then
-                make_writeable(Table)
-            end
-            if rawget(Table, str) ~= nil then
-                rawset(Table, str, val)
-            end
-        end
-    end
+
+if table.isfrozen(Table) then
+
+make_writeable(Table)
+
 end
 
-local function ActivateScript()
+            if rawget(Table, str) ~= nil then
+
+                rawset(Table, str, val)
+
+print("SetGarbageCollectionValue - Method Completed " .. str .. " Set To " .. tostring(val))
+
+            end
+
+        end
+
+    end
+
+end
+
     SetGcValue("Ammo", 999)
     SetGcValue("HeadshotMultiplier", 1000)
     SetGcValue("MaxSpread", 0)
-    SetGcValue("FireRate", 0)
+    SetGcValue("FireRate", 0.2)
     SetGcValue("AmmoPerMag", 999)
     SetGcValue("ReloadTime", 0)
-end
+SetGcValue("Health", 10000)
+SetGcValue("HealthPercent", 0)
+SetGcValue("HealingIncrement", 500)
+SetGcValue("HeadshotMultiplierIncrement", 5000)
 
-local ScreenGui = Instance.new("ScreenGui")
-ScreenGui.Name = "MobileActivatorGUI"
-ScreenGui.IgnoreGuiInset = true
-ScreenGui.Parent = CoreGui
-
-local Button = Instance.new("TextButton")
-Button.Size = UDim2.new(0, 140, 0, 38)
-Button.Position = UDim2.new(0.5, -70, 0.05, 0)
-Button.Text = "AKTYWUJ"
-Button.TextScaled = true
-Button.BackgroundColor3 = Color3.fromRGB(0,170,255)
-Button.TextColor3 = Color3.new(1,1,1)
-Button.Parent = ScreenGui
-
-Instance.new("UICorner", Button).CornerRadius = UDim.new(0,12)
-
-Button.MouseButton1Click:Connect(ActivateScript)
